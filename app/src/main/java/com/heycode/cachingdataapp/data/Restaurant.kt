2d72a9f2230e5d85d10a44d8164f206361f7ca7f
Kishan Kr Sharma <skishan781@gmail.com>
2021-05-18 14:33:25 +0530

    BugFixed: Data not showing

--- app/src/main/java/com/heycode/cachingdataapp/data/Restaurant.kt
+++ app/src/main/java/com/heycode/cachingdataapp/data/Restaurant.kt
@@ -1,10 +1,14 @@
 package com.heycode.cachingdataapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "restaurants")
 data class Restaurant(
     val name: String,
     val type: String,
     val logo: String,
     val address: String,
     val description: String,
-    val phone_number: String,
+    @PrimaryKey val phone_number: String,
 )
